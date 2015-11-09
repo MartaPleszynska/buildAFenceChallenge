514b03bbf1ac98293bb3ae0bcadb090a49394e58
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-11-09 09:32:12 +0000

    initial commit

---
+++ fence.php
@@ -0,0 +1,18 @@
+<?php
+
+class Post
+{
+    private $length = 1.50;
+}
+
+class Railing
+{
+    private $width = 0.10;
+}
+
+class Fence
+{
+    public $length;
+    public $numberOfPosts;
+    public $numberOfRailings;
+}
